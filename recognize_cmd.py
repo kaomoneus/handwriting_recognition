a004a1ec6c142208983851592b2bbc2c79a0580f
Stepan Dyatkovskiy (kaomoneus) <ml@dyatkovskiy.com>
2022-06-23 16:22:02 +0400

    Fixed vocabularies load in 'recognize' command.

--- recognize_cmd.py
+++ recognize_cmd.py
@@ -7,7 +7,7 @@ from keras.saving.save import load_model
 from config import CACHE_DIR_DEFAULT
 from dataset_utils import GroundTruthPathsItem, ROI, preprocess_dataset, tf_dataset
 from plot_utils import tf_plot_predictions
-from text_utils import load_vocabulary
+from text_utils import load_vocabulary, add_voc_args, parse_voc_args, Vocabulary
 
 
 def register_recognize_args(recognize_cmd: argparse.ArgumentParser):
@@ -27,6 +27,8 @@ def register_recognize_args(recognize_cmd: argparse.ArgumentParser):
         action="store_true"
     )
 
+    add_voc_args(recognize_cmd)
+
 
 def handle_recognize_cmd(args: argparse.Namespace):
     run_recognize(
@@ -34,10 +36,17 @@ def handle_recognize_cmd(args: argparse.Namespace):
         model_path=args.model,
         roi=args.roi,
         preprocess=args.preprocess,
+        vocabulary=parse_voc_args(args),
     )
 
 
-def run_recognize(img_path: str, model_path: str, roi: ROI, preprocess: bool):
+def run_recognize(
+    img_path: str,
+    model_path: str,
+    roi: ROI,
+    preprocess: bool,
+    vocabulary: Vocabulary,
+):
 
     ds = [GroundTruthPathsItem(
         str_value="",
@@ -53,9 +62,6 @@ def run_recognize(img_path: str, model_path: str, roi: ROI, preprocess: bool):
         cache_dir=CACHE_DIR_DEFAULT
     )
 
-    vocabulary = load_vocabulary(str(Path(model_path).with_suffix(".voc")))
     model = load_model(model_path)
 
     tf_plot_predictions(model, tf_dataset(ds, vocabulary), vocabulary)
-
-
